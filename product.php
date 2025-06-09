7f20325941364b50d913ac86f1fe675aef45c1fe
Mickey <michaelzewdu1996@gmail.com>
2025-06-09 14:12:42 +0300

    added a styling to the product.php page

--- product.php
+++ product.php
@@ -22,18 +22,23 @@ if ($result->num_rows == 0) {
 $product = $result->fetch_assoc();
 ?>
 
-<h2><?= htmlspecialchars($product['name']) ?></h2>
-
-<div class="product-detail">
+<div style="text-align: center; ">
+    <div style="display: inline-block; width: 300px; padding: 30px; vertical-align: top;">
+    <h2><?= htmlspecialchars($product['name']) ?></h2>
+    <p><?= nl2br(htmlspecialchars($product['description'])) ?></p>
+</div>
+<div class="product-detail" style="display: inline-block; padding: 20px">
     <img src="uploads/<?= htmlspecialchars($product['image']) ?>" alt="<?= htmlspecialchars($product['name']) ?>" width="300">
     <p><strong>Price:</strong> $<?= htmlspecialchars($product['price']) ?></p>
-    <p><?= nl2br(htmlspecialchars($product['description'])) ?></p>
-
-    <form method="post" action="cart.php">
+</div>
+<div style="display: inline-block; text-align: center; vertical-align: top; width: 250px; height: 100px; padding: 20px" >
+        <form method="post" action="cart.php">
         <input type="hidden" name="product_id" value="<?= $product['id'] ?>">
-        <input type="number" name="quantity" value="1" min="1">
-        <button type="submit" name="add_to_cart">Add to Cart</button>
+        Qunatity: <br>
+            <input type="number" name="quantity" value="1" min="1" style="border-radius: 4px; height: 30px; text-align: center;"><br>
+            <button id="addCart" type="submit" name="add_to_cart">Add to Cart</button><br><br>
     </form>
+    </div>
 </div>
 
 <?php include 'includes/footer.php'; ?>
